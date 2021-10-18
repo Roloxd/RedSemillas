34c36ae423a7d4541282114b9a9d87e711aefcd6
César Linares <cesarrlinaress@gmail.com>
2021-10-18 14:32:32 +0200

    Swift Mailer agregado (Pero no envia el correo)

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -15,4 +15,39 @@ class DefaultController extends AbstractController
             'controller_name' => 'DefaultController',
         ]);
     }
+
+    #[Route('/correo', name: 'correo')]
+    public function email($name = "Cesar", \Swift_Mailer $mailer)
+    {
+        $message = (new \Swift_Message('Hello Email'))
+            ->setFrom('cesar@etnonautas.com')
+            ->setTo('estudiantes.etnonautas@gmail.com')
+            ->setBody(
+                'Hola'
+                // $this->renderView(
+                //     // templates/emails/registration.html.twig
+                //     'emails/email.txt.twig',
+                //     ['name' => $name]
+                // )
+                // 'text/html'
+            )
+
+            // // you can remove the following code if you don't define a text version for your emails
+            // ->addPart(
+            //     $this->renderView(
+            //         // templates/emails/registration.txt.twig
+            //         'emails/registration.html.twig',
+            //         ['name' => $name]
+            //     ),
+            //     'text/html'
+            // )
+        ;
+
+        $mailer->send($message);
+
+        return $this->render('emails/registration.html.twig', [
+            'controller_name' => 'DefaultController',
+            'name' => $name,
+        ]);
+    }
 }
